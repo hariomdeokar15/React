347601520fc8a6adbc6ad9aa2235bc150f154004
hariomdeokar15 <hariomdeokar15@gmail.com>
2026-06-12 16:07:41 +0530

    new file added

---
+++ BASICS/src/components/Card.jsx
@@ -0,0 +1,9 @@
+
+export default function Card({ children }){
+  return(
+      <div>
+        { children }
+        <p>--------------------------</p>
+      </div>
+  )
+}
---
+++ BASICS/src/components/Student.jsx
@@ -0,0 +1,17 @@
+
+
+export default function Student(props){
+   
+
+  return(
+    <>
+     <p>name: {props.name}</p>
+     <p>course: {props.course}</p>
+     <p>city: {props.city}</p>
+     <p>college: {props.college}</p>
+     <p>----------------------------------</p>
+    </>
+     
+  )
+
+}
